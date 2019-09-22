27acf30e154b8c6d19e46b75c365ef13a7e3d649
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-09-22 17:23:16 +0900

    Finished home page and added projects  page

--- new-site/src/pages/index.js
+++ new-site/src/pages/index.js
@@ -4,7 +4,8 @@ import "./index.css"
 import headShot from "../images/headShot.jpg"
 import sendIcon from "../images/send.svg"
 import linkedinIcon from "../images/linkedinSquare.svg"
-// import { Link } from "gatsby"
+import { Helmet } from 'react-helmet'
+import { Link } from "gatsby"
 // import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
@@ -21,40 +22,43 @@ const IndexPage = () => (
   //   <Link to="/page-2/">Go to page 2</Link>
   // </Layout>
   <>
-    <main id="home-page-main">
-      <section id="contact-section">
-        <div>
-          <a href="mailto:andrewmichaelzamora@gmail.com">
-            <img id="contact-icon" src={sendIcon} alt="Send Message Paper Airplane"></img>
-            <span>Get in touch</span>
-          </a>
-        </div>
-      </section>
-      <section id="about-section">
-        <img id="headShot" src={headShot} alt="Andrew Zamora"></img>
-        <div class="about-text">
-          <h1>Hello,</h1>
-          <p>My name is</p>
-          <h1>Andrew<br></br> Zamora.</h1>
-          <p>I build websites.</p>
-        </div>
-      </section>
-      <nav>
-        <div class="github slant">
-          <p>See my code on
-                <a href="https://github.com/AndrewZamora" target="_blank" rel="noopener noreferrer">Github</a>
-          </p>
-        </div>
-        <div class="projects">
-          <a href="/projects">Projects</a>
-        </div>
-        <div class="linkedin">
-          <a href="https://www.linkedin.com/in/andrew-zamora-21058116" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon}
-            alt="linkedin logo"></img></a>
-        </div>
-      </nav>
-    </main>
+  <Helmet>
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"></link>
+  </Helmet>
+      <main id="home-page-main">
+        <section id="contact-section">
+          <div>
+            <a href="mailto:andrewmichaelzamora@gmail.com">
+              <img id="contact-icon" src={sendIcon} alt="Send Message Paper Airplane"></img>
+              <span>Get in touch</span>
+            </a>
+          </div>
+        </section>
+        <section id="about-section">
+          <img id="headShot" src={headShot} alt="Andrew Zamora"></img>
+          <div className="about-text">
+            <h1>Hello,</h1>
+            <p>My name is</p>
+            <h1>Andrew<br></br> Zamora.</h1>
+            <p>I build websites.</p>
+          </div>
+        </section>
+        <nav>
+          <div className="github slant">
+            <p>See my code on
+                <a href="https://github.com/AndrewZamora" target="_blank" rel="noopener noreferrer"> Github</a>
+            </p>
+          </div>
+          <div className="projects">
+            <Link to="/projects/">Projects</Link>
+          </div>
+          <div className="linkedin">
+            <a href="https://www.linkedin.com/in/andrew-zamora-21058116" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon}
+              alt="linkedin logo"></img></a>
+          </div>
+        </nav>
+      </main>
   </>
-)
-
-export default IndexPage
+    )
+    
+    export default IndexPage
